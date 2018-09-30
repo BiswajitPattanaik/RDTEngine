9df0740433ef68bf499db8ebd61b0560d7f1dd5b
biswajit563 <b.pattanaik563@gmail.com>
2018-09-30 13:50:08 +0530

    Adding All Files

---
+++ dotnet/test/firefox/NightlyFirefoxWebDriver.cs
@@ -0,0 +1,15 @@
+using OpenQA.Selenium.Remote;
+
+namespace OpenQA.Selenium.Firefox
+{
+    // This is a simple wrapper class to create a FirefoxDriver that
+    // uses the Marionette implementation and has no parameters in the
+    // constructor.
+    public class NightlyFirefoxWebDriver : FirefoxDriver
+    {
+        public NightlyFirefoxWebDriver(FirefoxDriverService service)
+            : base(service, new FirefoxOptions() { BrowserExecutableLocation = @"C:\Program Files (x86)\Nightly\firefox.exe" }, RemoteWebDriver.DefaultCommandTimeout)
+        {
+        }
+    }
+}
